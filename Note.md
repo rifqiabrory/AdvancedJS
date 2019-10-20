ca5ae40cef7d427041f08f9e8f07a7a231c2f7d8
rifqiabrory <rifqi.upm@gmail.com>
2019-10-20 11:07:11 +0700

    initial update

---
+++ Note.md
@@ -0,0 +1,5 @@
+Membuat Object pada javascript :
+1. Object Literal
+2. Function Declaration
+3. Constructor Function (keyword "new") 
+4. Object.create()
---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Membuat Object pada javascript :
+// 1. Object Literal
+// 2. Function Declaration
+// 3. Constructor Function (keyword "new") 
+// 4. Object.create()
+
+
+// 1. Object Literal
+let mahasiswa = {
+    //property , value
+    nama: 'Rifqi',
+    energy: 10,
+    //property, method
+    makan: () => {
+
+    }
+}
